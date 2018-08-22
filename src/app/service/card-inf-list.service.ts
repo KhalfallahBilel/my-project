0fd8ea637069b2e3bb7d56a1059b49519b0dd5d5
Lebbadfati <lebbad_fatima@yahoo.com>
2018-08-22 12:24:55 +0200

    ajouts des cards

--- src/app/service/card-inf-list.service.ts
+++ src/app/service/card-inf-list.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { CardInfList } from '../model/CardInfList';
 
 const MOCKCARD = [
-  new CardInfList('','',''),
-  new CardInfList('','',''),
-  new CardInfList('','',''),
-  new CardInfList('','',''),
-  new CardInfList('','',''),
-  new CardInfList('','','')
+  new CardInfList('./assets/images/Algerie3.jpg','Algerie','Voyage Organisé'),
+  new CardInfList('./assets/images/Nice2.jpg','Nice','Voyage Organisé'),
+  new CardInfList('./assets/images/tunisie1.jpg','Tunisie','Voyage Organisé'),
+  new CardInfList('./assets/images/Istamboul.jpg','Turquie','Voyage Organisé'),
+  new CardInfList('./assets/images/Maroc.jpg','Maroc','Voyage Organisé'),
+  new CardInfList('./assets/images/Dubai.jpg','Dubai','Voyage Organisé'),
+  new CardInfList('./assets/images/Malaisie.jpg','Malaisie','Voyage Organisé'),
+  new CardInfList('./assets/images/Paris.jpg','Paris','Voyage Organisé')
 ];
 
 @Injectable({
